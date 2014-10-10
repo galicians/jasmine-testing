63288f9ed25019fc18be5e5c320bedac74f29ed1
galicians <galicians@gmail.com>
2014-10-10 15:57:29 +0100

    making a calculator

---
+++ spec/addNumbersSpec.js
@@ -0,0 +1,25 @@
+
+
+// describe('adds two numbers', function {
+// 	it("adds numbers", function() {
+// 		expect(add(3,4)).toEqual(7);
+// 	});
+// });
+
+
+describe("calculator", function() {
+    it("plus", function() {
+        expect(addNumber(3,4)).toEqual(7);
+    });
+
+    it ("subtraction", function() {
+    	expect(sub(8,4)).toEqual(4);
+    });
+
+    it ("multiplication", function() {
+    	expect(xNumbers(10,2)).toEqual(20);
+    });
+
+    
+
+});
---
+++ src/addNumbers.js
@@ -0,0 +1,11 @@
+function addNumber (num1, num2) {
+	return num1 + num2;
+}
+
+function sub (num1, num2) {
+	return num1 - num2;
+}
+
+function xNumbers (num1, num2) {
+	return num1 * num2
+};
